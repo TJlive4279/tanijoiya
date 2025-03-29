3b6117485a00e577a287b57be1f888424ef0948c
TJlive4279 <tanveer.alam.joiya@gmail.com>
2025-03-29 21:24:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function generateSignal() {
-    const signals = ["BUY", "SELL", "HOLD"];
-    let signalData = [];
 
-    timeFrames.forEach(timeFrame => {
-        const randomSignal = signals[Math.floor(Math.random() * signals.length)];
-        signalData.push(${timeFrame}: ${randomSignal});
-    });
-
-    console.log(signalData); // یہ ڈیٹا کنسول میں دکھانے کے لیے ہے
-    return signalData;
-}
